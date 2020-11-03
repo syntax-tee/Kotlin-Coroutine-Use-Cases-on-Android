ad851b8dccaa9a635a3ea1db9d593a0c90e2d2c4
Taiye Emmanuel <taiyeo@remitly.com>
2020-11-03 17:57:36 +0100

    Perform Timeout And Retry

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
@@ -3,7 +3,7 @@ package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase6
 import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
-import com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase1.UiState.*
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
@@ -15,7 +15,6 @@ class RetryNetworkRequestViewModel(
         uiState.value = UiState.Loading
 
         viewModelScope.launch {
-
             val numberOfRetries = 2
             try {
                retry(numberOfRetries){
@@ -28,14 +27,21 @@ class RetryNetworkRequestViewModel(
         }
     }
 
-    private suspend fun <T> retry(numberOfTries:Int, block: suspend () -> T):T{
+    private suspend fun <T> retry(numberOfTries:Int,
+        initialDelayMillis:Long = 100,
+        maxDelayMillis:Long = 1000,
+        factor: Double = 2.0,
+        block: suspend () -> T):T{
+        var currentDelay = initialDelayMillis
         repeat(numberOfTries) {
             try {
-                return block()
+            return block()
             } catch (exception: Exception) {
-                Timber.e(exception)
+             Timber.e(exception)
             }
-        }
+         }
+        delay(currentDelay)
+        currentDelay = (currentDelay * factor).toLong().coerceAtLeast(maxDelayMillis)
         return  block()
     }
 
--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase7/TimeoutAndRetryViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase7/TimeoutAndRetryViewModel.kt
@@ -1,13 +1,69 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase7
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.*
+import timber.log.Timber
 
 class TimeoutAndRetryViewModel(
     private val api: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequest() {
+        val numberOfTries = 2
+        val timeout = 1000L
 
+        val oreoVersionDeferred = viewModelScope.async {
+            retry(numberOfTries, timeout) {
+                api.getAndroidVersionFeatures(27)
+            }
+        }
+
+        val pieVersionDeferred = viewModelScope.async {
+            retryWithTimeOut(numberOfTries, timeout) {
+                api.getAndroidVersionFeatures(28)
+            }
+        }
+
+        viewModelScope.launch {
+
+            try {
+                val versionFeatures = listOf(oreoVersionDeferred, pieVersionDeferred).awaitAll()
+                UiState.Success(versionFeatures)
+            } catch (exception: Exception) {
+                Timber.e(exception)
+                uiState.value = UiState.Error("Network error occurred")
+            }
+        }
     }
+
+
+    suspend fun <T> retryWithTimeOut(
+        numberOfRetries: Int,
+        timeout: Long,
+        block: suspend () -> T
+    ) = retry(numberOfRetries) {
+        withTimeout(timeout) {
+            block()
+        }
+    }
+
+
+    private suspend fun <T> retry(
+        numberOfRetries: Int,
+        delayBetweenRetries: Long = 100,
+        block: suspend () -> T
+    ): T {
+        repeat(numberOfRetries) {
+            try {
+                return block()
+            } catch (exception: java.lang.Exception) {
+                Timber.e(exception)
+            }
+            delay(delayBetweenRetries)
+        }
+        return block()
+    }
+
 }
